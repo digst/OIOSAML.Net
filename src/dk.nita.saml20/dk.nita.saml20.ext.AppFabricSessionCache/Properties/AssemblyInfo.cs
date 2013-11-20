62c900ecf83bd931aa51fbc97a1bb937932db535
mollekas <mollekas@migrateemail.com>
2013-11-20 13:40:26 +0000

    NuGet commit

--- src/dk.nita.saml20/dk.nita.saml20.ext.AppFabricSessionCache/Properties/AssemblyInfo.cs
+++ src/dk.nita.saml20/dk.nita.saml20.ext.AppFabricSessionCache/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("dk.nita.saml20.ext.AppFabricSessionCache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is an app fabric session provider implementation of the ISessions interface used in OIOSAML.NET (dk.nita.saml20)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Digitaliseringsstyrelsen")]
 [assembly: AssemblyProduct("dk.nita.saml20.ext.AppFabricSessionCache")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha4")]
--- src/dk.nita.saml20/dk.nita.saml20.ext.AppFabricSessionCache/readme.txt
+++ src/dk.nita.saml20/dk.nita.saml20.ext.AppFabricSessionCache/readme.txt
@@ -1,4 +1,6 @@
-The app fabric cache must be started when using the app fabric session provider. That cache can be started by follwoing the steps below:
+An AppFabric Cache cluster must be installed and up and running.
+
+The AppFabric Cache can be started by follwoing the steps below:
 
 1. Open "Caching Administration Windows PowerShell" as administrator (elevated mode).
 2. Run command Start-CacheCluster
