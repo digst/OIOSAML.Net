c25db4c13fb8708d08d05a07f6225bac8500b2ae
spe@schultz.dk <spe@schultz.dk>
2014-09-24 11:38:27 +0000

    Version 1.7.11 Enabled EnableViewStateMac, thereby mitigating potential exploit where an attacker may be able to upload and execute arbitrary code on the web server.

--- src/dk.nita.saml20/dk.nita.saml20/Bindings/HttpPostBindingBuilder.cs
+++ src/dk.nita.saml20/dk.nita.saml20/Bindings/HttpPostBindingBuilder.cs
@@ -26,13 +26,13 @@ namespace dk.nita.saml20.Bindings
         /// Initializes a new instance of the <see cref="HttpPostBindingBuilder"/> class.
         /// </summary>
         /// <param name="endpoint">The IdP endpoint that messages will be sent to.</param>
-        public HttpPostBindingBuilder(IDPEndPointElement endpoint) 
+        public HttpPostBindingBuilder(IDPEndPointElement endpoint)
         {
             _destinationEndpoint = endpoint;
             _action = SAMLAction.SAMLRequest;
             _relayState = string.Empty;
         }
-               
+
         #endregion
 
         #region Properties
@@ -59,7 +59,7 @@ namespace dk.nita.saml20.Bindings
         {
             get { return _relayState; }
             set { _relayState = value; }
-        }        
+        }
 
         private string _request;
 
@@ -89,13 +89,13 @@ namespace dk.nita.saml20.Bindings
             get { return _response; }
             set
             {
-                if (!string.IsNullOrEmpty(_request))                                    
+                if (!string.IsNullOrEmpty(_request))
                     throw new ArgumentException("Request property is already specified. Unable to set Response property.");
 
                 _response = value;
             }
-        }        
-        
+        }
+
         #endregion
 
         #region Page related functions
@@ -113,37 +113,36 @@ namespace dk.nita.saml20.Bindings
 
             Page p = new Page();
             p.EnableViewState = false;
-            p.EnableViewStateMac = false;
 
             p.Controls.Add(new LiteralControl("<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine));
             p.Controls.Add(new LiteralControl("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\r\n\r\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">" + Environment.NewLine));
-            
+
             HtmlHead head = new HtmlHead();
             head.Title = "SAML2.0 POST binding";
-            p.Controls.Add(head);            
+            p.Controls.Add(head);
 
             p.Controls.Add(new LiteralControl(Environment.NewLine + "<body onload=\"document.forms[0].submit()\">" + Environment.NewLine));
-            p.Controls.Add(new LiteralControl("<noscript><p><strong>Note:</strong> Since your browser does not support JavaScript, you must press the Continue button once to proceed.</p></noscript>"));                                   
+            p.Controls.Add(new LiteralControl("<noscript><p><strong>Note:</strong> Since your browser does not support JavaScript, you must press the Continue button once to proceed.</p></noscript>"));
 
             p.Controls.Add(new LiteralControl("<form action=\"" + _destinationEndpoint.Url + "\" method=\"post\"><div>"));
 
-            if(!string.IsNullOrEmpty(RelayState))
+            if (!string.IsNullOrEmpty(RelayState))
             {
                 HtmlInputHidden relayStateHidden = new HtmlInputHidden();
                 relayStateHidden.ID = "RelayState";
                 relayStateHidden.Name = "RelayState";
                 relayStateHidden.Value = RelayState;
                 p.Controls.Add(relayStateHidden);
-            }            
+            }
 
             HtmlInputHidden action = new HtmlInputHidden();
-            action.Name = Enum.GetName(typeof (SAMLAction), Action);
+            action.Name = Enum.GetName(typeof(SAMLAction), Action);
             action.ID = Enum.GetName(typeof(SAMLAction), Action);
             action.Value = Convert.ToBase64String(Encoding.UTF8.GetBytes(msg));
             p.Controls.Add(action);
 
             p.Controls.Add(new LiteralControl("<noscript><div><input type=\"submit\" value=\"Continue\"/></div></noscript>"));
-            p.Controls.Add(new LiteralControl("</div></form>"));                                      
+            p.Controls.Add(new LiteralControl("</div></form>"));
             p.Controls.Add(new LiteralControl(Environment.NewLine + "</body>" + Environment.NewLine + "</html>"));
 
             return p;
