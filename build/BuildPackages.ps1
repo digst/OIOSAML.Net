0158dc38dc29990cf6232fc9dd719f1d56a82974
afh@schultz.dk <afh@schultz.dk>
2017-09-06 10:02:09 +0000

    Added nuget source to build scripts

--- build/BuildPackages.ps1
+++ build/BuildPackages.ps1
@@ -1,28 +1,30 @@
-param([string] [parameter(Mandatory = $true)] $version, [switch] $pushPackages)
+param(
+    [string] [parameter(Mandatory = $true)] $version, 
+    [string] [parameter(Mandatory = $true)] $assemblyVersion, 
+    [switch] $pushPackages)
 
 $ErrorActionPreference = "Stop"
 
 if($pushPackages.IsPresent)
 {
     write-host "pushing package dk.nita.saml20" -ForegroundColor Yellow
-    .\nuget.exe push $("dk.nita.saml20.$version.nupkg")
+    .\nuget.exe push $("dk.nita.saml20.$version.nupkg") -Source https://www.nuget.org/api/v2/package
 
     write-host "pushing package dk.nita.saml20.ext.audit.log4net" -ForegroundColor Yellow
-    .\nuget.exe push $("dk.nita.saml20.ext.audit.log4net.$version.nupkg")
+    .\nuget.exe push $("dk.nita.saml20.ext.audit.log4net.$version.nupkg") -Source https://www.nuget.org/api/v2/package
     
     write-host "pushing package dk.nita.saml20.ext.sessionstore.sqlserver" -ForegroundColor Yellow
-    .\nuget.exe push $("dk.nita.saml20.ext.sessionstore.sqlserver.$version.nupkg")
+    .\nuget.exe push $("dk.nita.saml20.ext.sessionstore.sqlserver.$version.nupkg") -Source https://www.nuget.org/api/v2/package
 }
 else
 {
     write-host "Generating assembly versioning" -ForegroundColor Yellow
-    $assemblyversion = "$version.0"
 
     "using System.Reflection; 
 
     [assembly: AssemblyVersion(`"$assemblyversion`")]
     [assembly: AssemblyFileVersion(`"$assemblyversion`")]
-    [assembly: AssemblyInformationalVersion(`"$version`")]" | sc ..\src\dk.nita.saml20\CommonAssemblyInfo.cs
+    [assembly: AssemblyInformationalVersion(`"$assemblyversion`")]" | sc ..\src\dk.nita.saml20\CommonAssemblyInfo.cs
 
     write-host "Restoring nuget packages" -ForegroundColor Yellow
     .\nuget.exe restore ..\src\dk.nita.saml20
