ef9bcd189ab5145facb9311ffa0706980e5210b8
Morten B. Hansen <mobha@nets.eu>
2021-02-16 11:39:02 +0100

    NLRFIM-66 Adding IntegrationTest project

---
+++ src/dk.nita.saml20/IntegrationTest/WebsiteDemoTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+
+namespace IntegrationTest
+{
+    public class WebsiteDemoTest
+    {
+        [Fact]
+        public void LoginTest()
+        {
+            var serviceProviderEndpoint = ConfigurationManager.AppSettings["ServiceProviderEndpoint"];
+            var username = ConfigurationManager.AppSettings["IdpUsername"];
+            var password = ConfigurationManager.AppSettings["IdpPassword"];
+            
+            var options = new ChromeOptions();
+            options.AddArguments("headless");
+            options.AddArguments("allow-insecure-localhost");
+            
+            var driver = new ChromeDriver(options);
+            
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                
+                //Navigate to login page
+                driver.Navigate().GoToUrl(serviceProviderEndpoint);
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//*[text()='Test login']"))).Click();
+                
+                //Log in
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("ContentPlaceHolder_MitIdSimulatorControl_txtUsername")))
+                    .SendKeys(username);
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("ContentPlaceHolder_MitIdSimulatorControl_txtPassword")))
+                    .SendKeys(password);
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("ContentPlaceHolder_MitIdSimulatorControl_btnSubmit"))).Click();
+                
+                //Verify response
+                wait.Until(d => d.FindElement(By.XPath("//*[text()='SAML attributes']")));
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
