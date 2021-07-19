2fbb73bdc7de170be49e2c2deef091953dc5128c
Morten D. Bech <mdbec@nets.eu>
2021-07-19 17:59:49 +0200

    NLRFIM-118 Update XML namespace for OIO BPP

---
+++ src/dk.nita.saml20/dk.nita.saml20/Schema/BasicPrivilegeProfile/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace dk.nita.saml20.Schema.BasicPrivilegeProfile
+{
+    internal class Constants
+    {
+        internal const string XmlNamespace = "http://digst.dk/oiosaml/basic_privilege_profile";
+    }
+}
